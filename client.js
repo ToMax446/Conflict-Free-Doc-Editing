5f7a996d101cf04d8f7939c229774529fd7838cf
yakirbit <yakirbit@post.bgu.ac.il>
2022-01-02 14:36:56 +0200

    finish

--- client.js
+++ client.js
@@ -2,64 +2,60 @@ const net = require('net');
 const fs = require('fs');
 
 const LOCAL_UPDATES = 1;
+let COUNT_UPDATES = 0;
 
 class Client {
-	constructor(id, port, string, peers, string_operations) {
-		this.id = id;
-		this.port = port;
-		this.string = string;
-		this.oldString = string;
-		this.peers = peers;
-		this.string_operations = string_operations;
-		this.sockets = [];
-		this.timestamp = 0;
-		this.goodbyes = 0;
-	}
+    constructor(id, port, string, peers, string_operations) {
+        this.id = id;
+        this.port = port;
+        this.string = string;
+        this.peers = peers;
+        this.string_operations = string_operations;
+        this.sockets = [];
+        this.timestamp = 0;
+        this.goodbyes = 0;
+    }
 }
 
 class Peer {
-	constructor(id, host, port) {
-		this.id = id;
-		this.host = host;
-		this.port = port;
-	}
-}
-
-class Message {
-	constructor(from, text) {
-		this.from = from;
-		this.text = text;
-	}
+    constructor(id, host, port) {
+        this.id = id;
+        this.host = host;
+        this.port = port;
+    }
 }
 
 class Event {
-	constructor(timestamp, op, index, str) {
-		this.timestamp = timestamp;
-		this.op = op;
-		this.index = index;
-		this.str = str;
-	}
+    constructor(timestamp, id, op, orgString, newString) {
+        this.timestamp = timestamp;
+        this.id = id;
+        this.op = op;
+        this.orgString = orgString;
+        this.newString = newString;
+    }
 }
-const sleep = async function (ms) {
-	await new Promise((resolve) => setTimeout(resolve, ms));
+
+const close = () => {
+    client.goodbyes = client.goodbyes + 1;
+    if (client.goodbyes === client.peers.length + 1) {
+        console.log(`Client ${client.id} replica ${client.string}`);
+        console.log(`Client ${client.id} is exiting`);
+        client.server.close()
+        client.sockets.forEach(socket => socket.end())
+    }
 };
+
 // adding a method to the prototype of string to add at certain index
 String.prototype.addAt = function (index, text) {
-	return index !== undefined
-		? this.substring(0, index) + text + this.substring(index + text.length + 1)
-		: this + text;
+    return index !== undefined
+        ? this.substring(0, index) + text + this.substring(index + text.length + 1)
+        : this + text;
 };
 // adding a method to the prototype of string to add remove at certain index
 String.prototype.removeAt = function (index) {
-	return this.substring(0, index) + this.substring(index + 1);
+    return this.substring(0, index) + this.substring(index + 1);
 };
-close = () => {
-	if (client.goodbyes < peers.length) client.goodbyes++;
-	else {
-		console.log(string);
-		client.server.unref();
-	}
-};
+
 // File parsing
 let file = fs.readFileSync(process.argv[2], 'utf-8').split(/\r?\n/);
 file = file.reverse();
@@ -70,29 +66,22 @@ const operationQueue = [];
 file.pop(); // ""
 let peers = [];
 while (true) {
-	let line = file.pop();
-	if (line === '') {
-		break;
-	}
-	// let id = parseInt(splitLine[0]);
-	// let host = line.split(' ')[1];
-	// let port = parseInt(line.split(' ')[2]);
-	let [id, host, port] = line.split(' ');
-	let peer = new Peer(id, host, port);
-	console.log(id, host, port);
-	peers.push(peer);
+    let line = file.pop();
+    if (line === '') {
+        break;
+    }
+    let [id, host, port] = line.split(' ');
+    let peer = new Peer(id, host, port);
+    console.log(id, host, port);
+    peers.push(peer);
 }
 // file.pop(); // pop takes out the last item but we reversed
 file.shift(); // use shift to take the first item
 file.shift();
 let string_operations = [];
 while (file.length !== 0) {
-	// while (true) {
-	let line = file.pop();
-	// 	if (line === '') {
-	// 		break;
-	// 	}
-	string_operations.push(line);
+    let line = file.pop();
+    string_operations.push(line);
 }
 
 // Init client
@@ -100,160 +89,130 @@ let client = new Client(id, port, string, peers, string_operations);
 
 // The client connects via sockets to other peers with greater id
 for (const peer of client.peers) {
-	if (peer.id <= client.id) {
-		continue;
-	}
-	let socket = new net.Socket();
-	socket.on('data', handle); // what happen when receiving data from the socket
-	socket.on('connect', () => {
-		console.log(
-			'Connection from ::ffff:',
-			socket.localAddress,
-			'port',
-			socket.localPort
-		);
-		client.sockets.push(socket); // add peer socket to the pool of the client sockets
-		if (client.sockets.length === peers.length) {
-			eventLoop();
-		}
-	}); // what happen when socket connection is successfully established
-	socket.on('error', (e) => {
-		if (e.code === 'ECONNREFUSED') {
-			console.log(
-				`Connection ECONNREFUSED from Client ${client.id} to Peer ${peer.id}.`
-			);
-			setTimeout(() => {
-				socket.connect(peer.port, peer.host); // try to connect to the peer again
-			}, 1000);
-		}
-	}); // what happen when error occur, handle only when the connection refused
-	socket.connect(peer.port, peer.host); // try to connect to the peer
+    if (peer.id <= client.id) {
+        continue;
+    }
+    let socket = new net.Socket();
+    socket.on('data', handle); // what happen when receiving data from the socket
+    socket.on('connect', () => {
+        console.log(
+            'Connection from ::ffff:',
+            socket.localAddress,
+            'port',
+            socket.localPort
+        );
+        client.sockets.push(socket); // add peer socket to the pool of the client sockets
+        if (client.sockets.length === peers.length) {
+            operationLoop();
+        }
+    }); // what happen when socket connection is successfully established
+    socket.on('error', (e) => {
+        if (e.code === 'ECONNREFUSED') {
+            console.log(
+                `Connection ECONNREFUSED from Client ${client.id} to Peer ${peer.id}.`
+            );
+            setTimeout(() => {
+                socket.connect(peer.port, peer.host); // try to connect to the peer again
+            }, 1000);
+        }
+    }); // what happen when error occur, handle only when the connection refused
+    socket.connect(peer.port, peer.host); // try to connect to the peer
 }
-const updateEvent = (event) => {
-	// sets timestamps and pushes the event to the storage
-	timestamp = Math.max(event.timestamp, this.timestamp) + 1;
-	event.timestamp = this.timestamp;
-	operationQueue.push(event);
-	operationQueue.sort((firstEl, secondEl) => {
-		// make sure storage is sorted by ts
-		// sort the array by timestamp
-		firstEl[0] - secondEl[0];
-	});
-};
-const sendEvent = (event) => {
-	// emits the event to all peers through their sockets
-	for (const socket of client.sockets) socket.emit(event);
-};
 
 // The client generates an http server (as shown in class, here at page 88) in order to accept connections from clients with lower id (if exists)
 client.server = net.createServer((socket) => {
-	console.log(
-		'Connection from',
-		socket.remoteAddress,
-		'port',
-		socket.remotePort
-	);
-	socket.on('data', handle); // what happen when receiving data from the socket
-	socket.on('goodbye', close);
+    console.log(
+        'Connection from',
+        socket.remoteAddress,
+        'port',
+        socket.remotePort
+    );
+    socket.on('data', handle); // what happen when receiving data from the socket
 
-	// Listen to updateString events
-	// //prettier-ignore
-	// socket.on('updateEvent', insertOp);
-	// socket.on('deleteOp', deleteOp);
-
-	client.sockets.push(socket); // add peer socket to the pool of the client sockets
-	if (client.sockets.length === peers.length) {
-		eventLoop();
-	}
+    client.sockets.push(socket); // add peer socket to the pool of the client sockets
+    if (client.sockets.length === peers.length) {
+        operationLoop();
+    }
 });
 client.server.listen(client.port);
-const applyModification = (op, index, str) => {
-	string = op === 'insert' ? string.addAt(index, str) : string.removeAt(index);
-};
+
 // handle function when receiving new message from the socket (peer)
 function handle(buffer) {
-	let message = JSON.parse(buffer);
-	console.log(`Client ${client.id} ${message.text}`);
+    // let message = JSON.parse(buffer);
+    const event = JSON.parse(buffer);
+    if (event === "goodbye") {
+        close();
+        return;
+    }
+    console.log(`Client ${client.id} received an update operation <${event.op}, ${event.timestamp}> from client ${event.id}`);
+    client.timestamp = Math.max(event.timestamp, client.timestamp) + 1;
+    operationQueue.push(event);
+    operationQueue.sort((firstEl, secondEl) => {
+        // make sure storage is sorted by ts
+        // sort the array by timestamp
+        const ans = firstEl.timestamp - secondEl.timestamp;
+        return ans === 0 ? firstEl.id - secondEl.id : ans;
+    });
+    const [_, ...rest] = operationQueue;
+    if ((new Set(rest.map((e2) => e2.id).filter(id => id !== client.id)).size === client.peers.length)) {
+        const event1 = operationQueue.shift();
+        console.log(`Client ${client.id} removed operation  <${event1.op},  ${event1.timestamp}> from storage`);
+    }
+    console.log(`Client ${client.id} started merging, from ${client.timestamp} time stamp, on ${client.string}`);
+    const index = operationQueue.indexOf(event);
+    // client.string = index === 0 ? (operationQueue.length > 0 : operationQueue[index - 1].string) : operationQueue[index - 1].string; // index 0
+    if (index === 0) {
+        client.string = operationQueue.length > 1 ? operationQueue[1].orgString : client.string;
+    } else {
+        client.string = operationQueue[index - 1].newString;
+    }
+    for (const event of operationQueue.slice(index)) {
+        event.orgString = client.string;
+        if (event.op.includes("insert")) {
+            const [_, char, index] = event.op.split(' '); // if no index was given it will be undefined
+            if (index) {
+                client.string = client.string.substring(0, parseInt(index)) + char + client.string.substring(parseInt(index));
+            } else {
+                client.string = client.string + char;
+            }
+        } else {
+            const [_, index] = event.op.split(' ');
+            client.string = client.string.substring(0, parseInt(index)) + client.string.substring(parseInt(index) + 1);
+        }
+        event.newString = client.string;
+        console.log(`operation <${event.op}, ${event.timestamp}>, string: ${event.newString}`);
+    }
+    console.log(`Client ${client.id} ended merging with string ${client.string}, on timestamp ${client.timestamp}`);
+    console.log();
 }
-// FIX TODO BUG logging done wrong
-// const sendOpMessage = (op, timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`recieved an update operation ${op}, ${timestamp} from client ${client.id}`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const sendStartMergeMessage = (op, timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`started merging, from ${timestamp} timestamp on ${client.string}`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const sendEndMergeMessage = (op, timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`ended merging with string ${client.string}, on timestamp ${timestamp}`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const sendRemoveMessage = (op, timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`removed operation ${op},${timestamp} from storage`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const sendFinishedMessage = (timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`finished his local string modifications`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const sendFinishedMessage = (timestamp) => {
-// 	let message = new Message(
-// 		client.id,
-// 		`finished his local string modifications`
-// 	);
-// 	let buffer = JSON.stringify(message);
-// 	socket.write(buffer);
-// };
-// const insertOp = (timestamp, index, str) => {
-// 	// string = string.addAt(index, str);
-// };
-// const deleteOp = (timestamp, index) => {
-// 	// string = string.addAt(index, str);
-// 	operationQueue.push((timestamp, index, undefined, 'delete'));
-// 	operationQueue.sort((firstEl, secondEl) => {
-// 		// sort the array by timestamp
-// 		firstEl[0] - secondEl[0];
-// 	});
-// 	operationQueue.push((timestamp, index));
-// };
 
-for (const opString of string_operations) {
-	// goes through the string_operations and adds a task to the microtasks queue at the event loop
-	const [op, str, index] = opString.split(' '); // if no index was given it will be undefined
-	// new proto methods can handle index===undefined
-	setImmediate(() => {
-		applyModification(op, index, str);
-		const event = new Event(client.timestamp, op, index, str);
-		updateEvent(event);
-		sendEvent(event);
-	});
-	//as said in assignment "wait some time" after adding the task to the queue
-	//need to wrap below statement in async function
-	sleep(500); // check if this works
-}
-// Event loop
-function eventLoop() {
-	for (const socket of client.sockets) {
-		// emit (send) an event on the socket with the current timestamp and new string
-	}
+function operationLoop() {
+    if (string_operations.length > 0) {
+        const operation = client.string_operations.shift();
+        // const [op, char, index] = operation.split(' '); // if no index was given it will be undefined
+        setImmediate(() => {
+            client.timestamp = client.timestamp + 1;
+            const event = new Event(client.timestamp, client.id, operation);
+            COUNT_UPDATES = COUNT_UPDATES + 1;
+            if (COUNT_UPDATES === LOCAL_UPDATES) {
+                COUNT_UPDATES = 0;
+                for (const socket of client.sockets) socket.write(JSON.stringify(event));
+            }
+            // client.string = op === 'insert' ? client.string.addAt(index, char) : client.string.removeAt(index);
+            // event.string = client.string; // updated string
+            // operationQueue.push(event);
+            // operationQueue.sort((firstEl, secondEl) => {
+            //     // make sure storage is sorted by ts
+            //     // sort the array by timestamp
+            //     const ans = firstEl.timestamp - secondEl.timestamp;
+            //     return ans === 0 ? firstEl.id - secondEl.id : ans;
+            // });
+            handle(JSON.stringify(event));
+        });
+        setTimeout(() => operationLoop(), 1000);
+    } else {
+        console.log(`Client ${client.id} finished his local string modifications`);
+        close();
+        for (const socket of client.sockets) socket.write(JSON.stringify("goodbye"));
+    }
 }
